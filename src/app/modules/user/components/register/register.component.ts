2a4a26a90aa8b56fb04af257cf08b5ff5122fea7
rockts <rockts@sina.com>
2019-11-18 00:21:15 +0800

    完成《表单》学习

---
+++ src/app/modules/user/components/register/register.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent implements OnInit {
+  // registerForm = new FormGroup({
+  //   username: new FormControl('', Validators.required),
+  //   password: new FormControl('', [
+  //     Validators.required,
+  //     Validators.minLength(6),
+  //   ]),
+  // });
+
+  registerForm = this.formBuilder.group({
+    username: ['', Validators.required],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.registerForm.valueChanges.subscribe(value => {
+      console.log('Register:', value);
+    });
+
+    // this.registerForm.setValue({
+    //   username: 'leke_gao',
+    //   password: '111111',
+    // });
+
+    this.registerForm.statusChanges.subscribe(status => {
+      console.log('Status: ', status);
+    });
+  }
+
+  onSubmit() {
+    console.log('Submit: ', this.registerForm.value);
+  }
+}
