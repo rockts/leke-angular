665ae7d805a7cd59e80ce49ba4837f7002d90be2
rockts <rockts@sina.com>
2019-11-18 19:06:16 +0800

    Create module post

--- src/app/modules/post/models/post.model.ts
+++
@@ -1,3 +0,0 @@
-export class Post {
-  constructor(public id: number, public title: string, public body?: string) {}
-}
--- src/app/modules/post/post.module.ts
+++ src/app/modules/post/post.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PostComponent } from './post.component';
-import { PostItemComponent } from './components/post-item/post-item.component';
-import { PostDetailsComponent } from './components/post-details/post-details.component';
-import { AppRoutingModule } from '../../app-routing.module';
+
+
 
 @NgModule({
-  declarations: [PostComponent, PostItemComponent, PostDetailsComponent],
-  imports: [CommonModule, AppRoutingModule],
-  exports: [PostComponent],
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
 })
-export class PostModule {}
+export class PostModule { }
