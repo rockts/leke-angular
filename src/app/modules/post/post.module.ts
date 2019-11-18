a17aee3d2132786b0c92f05ef92dc50c2df63768
rockts <rockts@sina.com>
2019-11-18 22:01:55 +0800

    Create component post-details

--- src/app/modules/post/post.module.ts
+++ src/app/modules/post/post.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostComponent } from './post.component';
 import { PostItemComponent } from './compoents/post-item/post-item.component';
+import { PostDetailsComponent } from './compoents/post-details/post-details.component';
 
 @NgModule({
-  declarations: [PostComponent, PostItemComponent],
+  declarations: [PostComponent, PostItemComponent, PostDetailsComponent],
   imports: [CommonModule],
   exports: [PostComponent],
 })
